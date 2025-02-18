2f0a3df07ede040b4d4c4afab0a696d5218aeb3e
Elizabeth <elizabeth-g-stone@uiowa.edu>
2025-02-18 15:03:16 -0600

    OLED testing/some setup

--- calendurr/src/main.cpp
+++ calendurr/src/main.cpp
@@ -1,4 +1,17 @@
 #include <ArduinoBLE.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3D
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 
 BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // Bluetooth® Low Energy LED Service
 
@@ -7,9 +20,22 @@ BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214
 
 const int ledPin = LED_BUILTIN; // pin to use for the LED
 
+
+
+/*-----------------  SETUP FCN   -------------------*/
 void setup() {
   Serial.begin(9600);
   while (!Serial);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0,0);
 
   // set LED pin to output mode
   pinMode(ledPin, OUTPUT);
@@ -17,7 +43,8 @@ void setup() {
   // begin initialization
   if (!BLE.begin()) {
     Serial.println("starting Bluetooth® Low Energy module failed!");
-
+    display.println(F("BLE failure"));
+    display.display();
     while (1);
   }
 
@@ -38,25 +65,44 @@ void setup() {
   BLE.advertise();
 
   Serial.println("BLE LED Peripheral");
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.println(F("miau"));
+    display.display();
 }
 
+
+/*-----------------  MAIN LOOP   -------------------*/
 void loop() {
   // listen for Bluetooth® Low Energy peripherals to connect:
   BLEDevice central = BLE.central();
 
+
   // if a central is connected to peripheral:
   if (central) {
     Serial.print("Connected to central: ");
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.println(F("Connected to BLE"));
+    display.display();
     // print the central's MAC address:
     Serial.println(central.address());
 
     // while the central is still connected to peripheral:
   while (central.connected()) {
         if (switchCharacteristic.written()) {
-          if (switchCharacteristic.value()) {   
+          if (switchCharacteristic.value()) {  
+            display.clearDisplay();
+            display.setCursor(0,0);
+            display.println(F("LED on")); 
+            display.display();
             Serial.println("LED on");
             digitalWrite(ledPin, LOW); // changed from HIGH to LOW       
-          } else {                              
+          } else {                       
+            display.clearDisplay();
+            display.setCursor(0,0);
+            display.println(F("LED off"));  
+            display.display();     
             Serial.println(F("LED off"));
             digitalWrite(ledPin, HIGH); // changed from LOW to HIGH     
           }
@@ -65,6 +111,10 @@ void loop() {
 
     // when the central disconnects, print it out:
     Serial.print(F("Disconnected from central: "));
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.println(F("disconnected from BLE"));
+    display.display();
     Serial.println(central.address());
   }
 }
