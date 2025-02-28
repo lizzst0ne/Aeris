171a1b5e981bc8986cc06e84b99f6f67399e4853
Elizabeth <elizabeth-g-stone@uiowa.edu>
2025-02-28 17:42:31 -0600

    working on updating bluetooth to work with this board :p
    but im too eepy rn to think 😔

--- calendurr/src/main.cpp
+++ calendurr/src/main.cpp
@@ -1,4 +1,4 @@
-// #include <ArduinoBLE.h>
+#include <bluefruit.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -44,10 +44,24 @@ int delY;
 unsigned char coordsArray[X_LIMIT][Y_LIMIT] = {0};
 
 void read();
-
 void button();
 
+void startAdv();
+void prph_connect_callback(uint16_t conn_handle);
+void prph_disconnect_callback(uint16_t conn_handle, uint8_t reason);
+void prph_bleuart_rx_callback(uint16_t conn_handle);
+
+void scan_callback(ble_gap_evt_adv_report_t* report);
+void cent_connect_callback(uint16_t conn_handle);
+void cent_disconnect_callback(uint16_t conn_handle, uint8_t reason);
+
+void cent_bleuart_rx_callback(BLEClientUart& cent_uart);
+
 // Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+// uart service
+BLEUart bleuart;
+BLEClientUart clientUart;
 
 
 // BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // Bluetooth® Low Energy LED Service
@@ -64,26 +78,28 @@ void setup() {
   Serial.begin(115200);
   while (!Serial);
 
+
+//SENSOR SETUP
   // set top and bottom pins to output, and initialize to low
-  pinMode(TOP_R, OUTPUT);
-  pinMode(TOP_L, OUTPUT);
-  pinMode(BOTTOM_L, OUTPUT);
-  pinMode(BOTTOM_R, OUTPUT);
-  digitalWrite(TOP_R, LOW);
-  digitalWrite(TOP_L, LOW);
-  digitalWrite(BOTTOM_L, LOW);
-  digitalWrite(BOTTOM_R, LOW);
-  // set sense pin to output
-  pinMode(SENSE, INPUT);
+  // pinMode(TOP_R, OUTPUT);
+  // pinMode(TOP_L, OUTPUT);
+  // pinMode(BOTTOM_L, OUTPUT);
+  // pinMode(BOTTOM_R, OUTPUT);
+  // digitalWrite(TOP_R, LOW);
+  // digitalWrite(TOP_L, LOW);
+  // digitalWrite(BOTTOM_L, LOW);
+  // digitalWrite(BOTTOM_R, LOW);
+  // // set sense pin to output
+  // pinMode(SENSE, INPUT);
 
-  pinMode(BUTTON, INPUT);
+  // pinMode(BUTTON, INPUT);
 
-  analogReadResolution(12);
-  analogReference(AR_INTERNAL);
+  // analogReadResolution(12);
+  // analogReference(AR_INTERNAL);
 
-  last_x = 0;
-  last_y = 0;
-
+  // last_x = 0;
+  // last_y = 0;
+//END SENSOR SETUP
 
   // if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
   //   Serial.println(F("SSD1306 allocation failed"));
@@ -98,11 +114,44 @@ void setup() {
   // // set LED pin to output mode
   // pinMode(ledPin, OUTPUT);
 
+  Bluefruit.begin(1, 1);
+  Bluefruit.setTxPower(4);
+
+  // Callbacks
+  Bluefruit.Periph.setConnectCallback(prph_connect_callback);
+  Bluefruit.Periph.setDisconnectCallback(prph_disconnect_callback);
+
+  Bluefruit.Central.setConnectCallback(cent_connect_callback);
+  Bluefruit.Central.setDisconnectCallback(cent_disconnect_callback);
+
+  bleuart.begin();
+  bleuart.setRxCallback(prph_bleuart_rx_callback);
+
+  clientUart.begin();
+  clientUart.setRxCallback(cent_bleuart_rx_callback);
+
+  /* Start Central Scanning
+   * - Enable auto scan if disconnected
+   * - Interval = 100 ms, window = 80 ms
+   * - Filter only accept bleuart service
+   * - Don't use active scan
+   * - Start(timeout) with timeout = 0 will scan forever (until connected)
+   */
+  Bluefruit.Scanner.setRxCallback(scan_callback);
+  Bluefruit.Scanner.restartOnDisconnect(true);
+  Bluefruit.Scanner.setInterval(160, 80); // in unit of 0.625 ms
+  Bluefruit.Scanner.filterUuid(bleuart.uuid);
+  Bluefruit.Scanner.useActiveScan(false);
+  Bluefruit.Scanner.start(0);                   // 0 = Don't stop scanning after n seconds
+
+  // Set up and start advertising
+  startAdv();
+
   // // begin initialization
   // if (!BLE.begin()) {
   //   Serial.println("starting Bluetooth® Low Energy module failed!");
-  //   display.println(F("BLE failure"));
-  //   display.display();
+  //   // display.println(F("BLE failure"));
+  //   // display.display();
   //   while (1);
   // }
 
@@ -133,19 +182,21 @@ void setup() {
 
 /*-----------------  MAIN LOOP   -------------------*/
 void loop() {
+//SENSOR ACTIONS TO BE UNCOMMENTED LATER
+  // read();
 
-  read();
+  // if(!digitalRead(BUTTON)){
+  //   button();
+  // }
 
-  if(!digitalRead(BUTTON)){
-    button();
-  }
+  // char test[40];
 
-  char test[40];
+  // sprintf(test, "X:%d  Y:%d", x_pos, y_pos);
 
-  sprintf(test, "X:%d  Y:%d", x_pos, y_pos);
+  // Serial.println(test);
+  // delay(10);
+//END SENSOR ACTIONS
 
-  Serial.println(test);
-  delay(10);
 
   // // listen for Bluetooth® Low Energy peripherals to connect:
   // BLEDevice central = BLE.central();
@@ -241,4 +292,139 @@ void button(){
       Serial.print("\n");
   }
 
+}
+
+void startAdv(void)
+{
+  // Advertising packet
+  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
+
+  // Include bleuart 128-bit uuid
+  Bluefruit.Advertising.addService(bleuart);
+
+  // Secondary Scan Response packet (optional)
+  // Since there is no room for 'Name' in Advertising packet
+  Bluefruit.ScanResponse.addName();
+
+  /* Start Advertising
+   * - Enable auto advertising if disconnected
+   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
+   * - Timeout for fast mode is 30 seconds
+   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
+   *
+   * For recommended advertising interval
+   * https://developer.apple.com/library/content/qa/qa1931/_index.html
+   */
+  Bluefruit.Advertising.restartOnDisconnect(true);
+  Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
+  Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
+  Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
+}
+
+/*------------------------------------------------------*/
+/*  Peripheral                                          */
+/*------------------------------------------------------*/
+void prph_connect_callback(uint16_t conn_handle){
+  // reference to current connection
+  BLEConnection* connection = Bluefruit.Connection(conn_handle);
+
+  char peer_name[32] = {0};
+  connection -> getPeerName(peer_name, sizeof(peer_name));
+
+  Serial.print("[Prph] Connected to ");
+  Serial.println(peer_name);
+}
+
+void prph_disconnect_callback(uint16_t conn_handle, uint8_t reason){
+  (void) conn_handle;
+  (void) reason;
+
+  Serial.println();
+  Serial.println("[Prph] Disconnected");
+}
+
+void prph_bleuart_rx_callback(uint16_t conn_handle)
+{
+  (void) conn_handle;
+  
+  // Forward data from Mobile to our peripheral
+  char str[20+1] = { 0 };
+  bleuart.read(str, 20);
+
+  Serial.print("[Prph] RX: ");
+  Serial.println(str);  
+
+  if ( clientUart.discovered() )
+  {
+    clientUart.print(str);
+  }else
+  {
+    bleuart.println("[Prph] Central role not connected");
+  }
+}
+
+/*------------------------------------------------------------------*/
+/* Central
+ *------------------------------------------------------------------*/
+void scan_callback(ble_gap_evt_adv_report_t* report)
+{
+  // Since we configure the scanner with filterUuid()
+  // Scan callback only invoked for device with bleuart service advertised  
+  // Connect to the device with bleuart service in advertising packet  
+  Bluefruit.Central.connect(report);
+}
+
+void cent_connect_callback(uint16_t conn_handle)
+{
+  // Get the reference to current connection
+  BLEConnection* connection = Bluefruit.Connection(conn_handle);
+
+  char peer_name[32] = { 0 };
+  connection->getPeerName(peer_name, sizeof(peer_name));
+
+  Serial.print("[Cent] Connected to ");
+  Serial.println(peer_name);;
+
+  if ( clientUart.discover(conn_handle) )
+  {
+    // Enable TXD's notify
+    clientUart.enableTXD();
+  }else
+  {
+    // disconnect since we couldn't find bleuart service
+    Bluefruit.disconnect(conn_handle);
+  }  
+}
+
+void cent_disconnect_callback(uint16_t conn_handle, uint8_t reason)
+{
+  (void) conn_handle;
+  (void) reason;
+  
+  Serial.println("[Cent] Disconnected");
+}
+
+/**
+ * Callback invoked when uart received data
+ * @param cent_uart Reference object to the service where the data 
+ * arrived. In this example it is clientUart
+ */
+void cent_bleuart_rx_callback(BLEClientUart& cent_uart)
+{
+  char str[20+1] = { 0 };
+  cent_uart.read(str, 20);
+      
+  Serial.print("[Cent] RX: ");
+  Serial.println(str);
+
+  if ( bleuart.notifyEnabled() )
+  {
+    // Forward data from our peripheral to Mobile
+    bleuart.print( str );
+  }else
+  {
+    // response with no prph message
+    clientUart.println("[Cent] Peripheral role not connected");
+  }  
 }
